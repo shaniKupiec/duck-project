16cc4002ddc2fcc101fb07bd5325eefd5914995b
shani <shani@webiks.com>
2023-07-31 09:12:16 +0300

    fix img problem

--- src/components/DuckItem.jsx
+++ src/components/DuckItem.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
 
+import duckImage1 from '../assets/images/ducks/bachelor.png';
+import duckImage2 from '../assets/images/ducks/couple.png';
+import duckImage3 from '../assets/images/ducks/doctor.png';
+import duckImage4 from '../assets/images/ducks/unicorn.png';
+
 
 function DuckItem({ duck }) {
-    const imageFileName = duck.src;
+    let imageSrc;
+    switch (duck.src) {
+      case 'bachelor.png':
+        imageSrc = duckImage1;
+        break;
+      case 'couple.png':
+        imageSrc = duckImage2;
+        break;
+      case 'doctor.png':
+        imageSrc = duckImage3;
+        break;
+      case 'unicorn.png':
+        imageSrc = duckImage4;
+        break;
+      default:
+        imageSrc = null;
+    }
 
     return (
         <div>
-            {/* <img src={require(`./images/ducks/${imageFileName}`).default} alt={duck.occupation} /> */}
+             {imageSrc && <img src={imageSrc} alt={duck.occupation} />}
             <div>occupation : {duck.occupation}</div>
             <div>number : {duck.id}</div>
         </div>
